dead16aa7b4ee0895c7a33075aea7389fcea2b70
David <david@tangentdigitalagency.com>
2021-02-11 11:38:54 -0700

    thank you

--- src/components/S9Final.jsx
+++ src/components/S9Final.jsx
@@ -21,6 +21,7 @@ class S9Final extends Component {
 			<div className="container pt-0 main-content-container">
 				<h1 className="center"> You Are On Your Way To Saving!</h1>
 				<br></br>
+				<h3 className="center"> A Quantum Agent will be with you very shortly!</h3>
                 <div id=""> 
 			 <UncontrolledLottie /> 
 				
