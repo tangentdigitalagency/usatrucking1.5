068f88d867f8b9eb6148055bac4724a027b89d35
David Verduzco <67717053+tangentdigitalagency@users.noreply.github.com>
2021-10-14 22:48:21 -0700

    Text update

--- src/components/Final.jsx
+++ src/components/Final.jsx
@@ -23,7 +23,6 @@ class S9Final extends Component {
 				<h1 className="center"> You Are On Your Way To Saving on Your <b>Business</b> Insurance</h1>
 				<br></br>
 				<p> 
-  If now isn't a good time to talk to an Agent about getting the best price on insrance for your business, you can call us at <a href="tel+18553361823"><b>(855) 336-1823</b> </a>at your convenience. We look foward to helping you save.
 </p>
 
                 <div id="target"> 
